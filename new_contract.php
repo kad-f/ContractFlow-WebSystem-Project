e1f7e54353133a9b4fd2194175c47096f9f4ce32
Keyan Andy Delgado <96453690+2ez4Keyan@users.noreply.github.com>
2023-11-10 13:51:50 +0800

    UPDATED

--- new_contract.php
+++ new_contract.php
@@ -122,7 +122,7 @@ include('update_notifications.php');
                     <input type="text" id="vendor_contact" name="vendor_contact"
                         placeholder="Enter Vendor Contact number">
                 </li>
-            </ul> 
+            </ul>
         </fieldset>
         <fieldset>
             <legend>Service Delivery Manager</legend>
@@ -226,7 +226,7 @@ if (isset($_POST['create_contract'])) {
     mysqli_query($conn, $insert_expiration);
     $expiration_id = mysqli_insert_id($conn);
 
-    $insert_contract = "INSERT INTO contract(contract_no, type_id, category_id, description, date_of_agreement, language_id, supplier_name, country_id, life_of_contract, vendor_id, sdm_id, sdm_remarks, currency_id, annual_spend, payment_type, status, expiration_id) VALUES('$reference_num', '$contract_type', '$category', '$description', '$date', '$language', '$supplier', '$country', '$life', '$vendor_id', '$sdm_id', '$remarks', '$currency', '$spend', '$payment_type', '$status', '$expiration_id')";
+    $insert_contract = "INSERT INTO contract(contract_no, type_id, category_id, description, date_of_agreement, supplier_name, life_of_contract, vendor_id, sdm_id, sdm_remarks, annual_spend, payment_type, status, expiration_id) VALUES('$reference_num', '$contract_type', '$category', '$description', '$date', '$language', '$supplier', '$country', '$life', '$vendor_id', '$sdm_id', '$remarks', '$currency', '$spend', '$payment_type', '$status', '$expiration_id')";
     $result_contract = mysqli_query($conn, $insert_contract);
 
     if ($result_contract) {
--- sql/e-contract.sql
+++ sql/e-contract.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2017 at 08:04 AM
--- Server version: 10.1.10-MariaDB
--- PHP Version: 5.6.19
+-- Generation Time: Nov 10, 2023 at 06:50 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `e-contract`
 --
-CREATE DATABASE IF NOT EXISTS `contract` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `e-contract`;
 
 -- --------------------------------------------------------
 
@@ -28,11 +27,10 @@ USE `e-contract`;
 -- Table structure for table `category`
 --
 
-DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `category_id` int(2) NOT NULL,
   `category` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -40,38 +38,23 @@ CREATE TABLE `category` (
 -- Table structure for table `contract`
 --
 
-DROP TABLE IF EXISTS `contract`;
 CREATE TABLE `contract` (
   `contract_no` varchar(12) NOT NULL,
   `type_id` int(2) NOT NULL,
   `category_id` int(2) NOT NULL,
   `description` text NOT NULL,
   `date_of_agreement` date NOT NULL,
-  `language_id` int(2) NOT NULL,
   `supplier_name` varchar(255) NOT NULL,
-  `country_id` int(2) NOT NULL,
   `life_of_contract` double(4,2) NOT NULL,
   `vendor_id` int(12) NOT NULL,
   `sdm_id` int(12) NOT NULL,
   `sdm_remarks` text NOT NULL,
-  `currency_id` int(2) NOT NULL,
   `annual_spend` int(12) NOT NULL,
+  `payment_type` varchar(255) DEFAULT NULL,
   `payment_terms` text NOT NULL,
   `status` varchar(255) NOT NULL,
   `expiration_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `country`
---
-
-DROP TABLE IF EXISTS `country`;
-CREATE TABLE `country` (
-  `country_id` int(2) NOT NULL,
-  `country` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -79,11 +62,10 @@ CREATE TABLE `country` (
 -- Table structure for table `currency`
 --
 
-DROP TABLE IF EXISTS `currency`;
 CREATE TABLE `currency` (
   `currency_id` int(2) NOT NULL,
   `currency` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -91,7 +73,6 @@ CREATE TABLE `currency` (
 -- Table structure for table `expiration`
 --
 
-DROP TABLE IF EXISTS `expiration`;
 CREATE TABLE `expiration` (
   `expiration_id` int(11) NOT NULL,
   `contract_no` varchar(12) NOT NULL,
@@ -101,7 +82,14 @@ CREATE TABLE `expiration` (
   `termination_rights` text NOT NULL,
   `assignment_provision` text NOT NULL,
   `notified` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `expiration`
+--
+
+INSERT INTO `expiration` (`expiration_id`, `contract_no`, `date`, `status_days`, `renewal_provision_id`, `termination_rights`, `assignment_provision`, `notified`) VALUES
+(2, '123', '2023-11-30', 0, 0, 'qwe', 'qwe', 1);
 
 -- --------------------------------------------------------
 
@@ -109,7 +97,6 @@ CREATE TABLE `expiration` (
 -- Table structure for table `invoice`
 --
 
-DROP TABLE IF EXISTS `invoice`;
 CREATE TABLE `invoice` (
   `id` int(12) NOT NULL,
   `contract_no` varchar(12) NOT NULL,
@@ -117,7 +104,7 @@ CREATE TABLE `invoice` (
   `file` text NOT NULL,
   `amount` int(12) NOT NULL,
   `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -125,7 +112,6 @@ CREATE TABLE `invoice` (
 -- Table structure for table `issues`
 --
 
-DROP TABLE IF EXISTS `issues`;
 CREATE TABLE `issues` (
   `id` int(12) NOT NULL,
   `contract_no` varchar(12) NOT NULL,
@@ -133,7 +119,7 @@ CREATE TABLE `issues` (
   `description` text NOT NULL,
   `issuer_name` varchar(255) NOT NULL,
   `issuer_role` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -141,11 +127,10 @@ CREATE TABLE `issues` (
 -- Table structure for table `language`
 --
 
-DROP TABLE IF EXISTS `language`;
 CREATE TABLE `language` (
   `language_id` int(2) NOT NULL,
   `language` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -153,12 +138,89 @@ CREATE TABLE `language` (
 -- Table structure for table `notice_period`
 --
 
-DROP TABLE IF EXISTS `notice_period`;
 CREATE TABLE `notice_period` (
   `contract_no` varchar(12) NOT NULL,
   `date` date NOT NULL,
   `description` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `contract_no` varchar(12) NOT NULL,
+  `status` int(1) NOT NULL,
+  `notification_text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_type`
+--
+
+CREATE TABLE `payment_type` (
+  `payment_type_id` int(11) NOT NULL,
+  `payment_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payment_type`
+--
+
+INSERT INTO `payment_type` (`payment_type_id`, `payment_name`) VALUES
+(1, 'Cash'),
+(2, 'Check'),
+(3, 'Credit Card'),
+(4, 'Installments'),
+(5, 'PayPal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permission_role`
+--
+
+CREATE TABLE `permission_role` (
+  `permission_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `permission_role`
+--
+
+INSERT INTO `permission_role` (`permission_id`, `name`, `description`) VALUES
+(1, 'user_management_access', 'Access to user management'),
+(2, 'permission_create', 'Create permissions'),
+(3, 'permission_edit', 'Edit permissions'),
+(4, 'permission_show', 'View permissions'),
+(5, 'permission_delete', 'Delete permissions'),
+(6, 'permission_access', 'Access to permission management'),
+(7, 'role_create', 'Create roles'),
+(8, 'role_edit', 'Edit roles'),
+(9, 'role_show', 'View roles'),
+(10, 'role_delete', 'Delete roles'),
+(11, 'role_access', 'Access to role management'),
+(12, 'user_create', 'Create users'),
+(13, 'user_edit', 'Edit users'),
+(14, 'user_show', 'View users'),
+(15, 'user_delete', 'Delete users'),
+(16, 'user_access', 'Access to user management'),
+(17, 'contract_create', 'Create contracts'),
+(18, 'contract_edit', 'Edit contracts'),
+(19, 'contract_show', 'View contracts'),
+(20, 'contract_delete', 'Delete contracts'),
+(21, 'contract_access', 'Access to contract management'),
+(22, 'profile_password_edit', 'Edit user profile and password'),
+(23, 'can_add_notice_period', 'Add notice period to contracts'),
+(24, 'can_add_review', 'Add reviews to contracts'),
+(25, 'can_settle_issues', 'Settle issues with contracts'),
+(26, 'can_create_invoice', 'Create invoices for contracts');
 
 -- --------------------------------------------------------
 
@@ -166,11 +228,10 @@ CREATE TABLE `notice_period` (
 -- Table structure for table `renewal_provision`
 --
 
-DROP TABLE IF EXISTS `renewal_provision`;
 CREATE TABLE `renewal_provision` (
   `renewal_provision_id` int(2) NOT NULL,
   `renewal_provision` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -178,12 +239,11 @@ CREATE TABLE `renewal_provision` (
 -- Table structure for table `review`
 --
 
-DROP TABLE IF EXISTS `review`;
 CREATE TABLE `review` (
   `contract_no` varchar(12) NOT NULL,
   `reviewer_name` varchar(255) NOT NULL,
   `reviewer_comments` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -191,13 +251,19 @@ CREATE TABLE `review` (
 -- Table structure for table `service_delivery_manager`
 --
 
-DROP TABLE IF EXISTS `service_delivery_manager`;
 CREATE TABLE `service_delivery_manager` (
   `sdm_id` int(12) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone_no` varchar(13) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `service_delivery_manager`
+--
+
+INSERT INTO `service_delivery_manager` (`sdm_id`, `name`, `email`, `phone_no`) VALUES
+(3, 'qwe', 'qwe@gmail.com', '123');
 
 -- --------------------------------------------------------
 
@@ -205,11 +271,10 @@ CREATE TABLE `service_delivery_manager` (
 -- Table structure for table `type`
 --
 
-DROP TABLE IF EXISTS `type`;
 CREATE TABLE `type` (
   `type_id` int(2) NOT NULL,
   `type` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -217,22 +282,21 @@ CREATE TABLE `type` (
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(4) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(42) NOT NULL,
   `role_id` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role_id`) VALUES
-(1, 'Manish', 'manish@kontrak.com', 'Kontrak@1', 0),
-(2, 'Ankita', 'ankita@kontrak.com', 'Kontrak@1', 1);
+(1, 'admin', 'admin@gmail.com', '123', 1),
+(2, 'Ankita', 'ankita@kontrak.com', 'Kontrak@1', 2);
 
 -- --------------------------------------------------------
 
@@ -240,11 +304,19 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `role_id`) VALUES
 -- Table structure for table `user_role`
 --
 
-DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
-  `role_id` int(1) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `role_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`role_id`, `name`) VALUES
+(1, 'admin'),
+(2, 'client'),
+(3, 'service_delivery_manager');
 
 -- --------------------------------------------------------
 
@@ -252,33 +324,23 @@ CREATE TABLE `user_role` (
 -- Table structure for table `vendor`
 --
 
-DROP TABLE IF EXISTS `vendor`;
 CREATE TABLE `vendor` (
   `vendor_id` int(12) NOT NULL,
   `contact_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone_no` varchar(13) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-
--- --------------------------------------------------------
 --
--- Table structure for Notifications `notification`
+-- Dumping data for table `vendor`
 --
-DROP TABLE IF EXISTS `notification`;
-CREATE TABLE `notification` (
-`contract_no` varchar(12) NOT NULL,
-`status` int(1) NOT NULL,
-`notification_text` text NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+INSERT INTO `vendor` (`vendor_id`, `contact_name`, `email`, `phone_no`) VALUES
+(6, 'TEST', 'test@gmail.com', '09262408442');
 
 --
 -- Indexes for dumped tables
 --
-
--- --------------------------------------------------------
 
 --
 -- Indexes for table `category`
@@ -291,12 +353,6 @@ ALTER TABLE `category`
 --
 ALTER TABLE `contract`
   ADD UNIQUE KEY `contract_no` (`contract_no`);
-
---
--- Indexes for table `country`
---
-ALTER TABLE `country`
-  ADD PRIMARY KEY (`country_id`);
 
 --
 -- Indexes for table `currency`
@@ -329,6 +385,24 @@ ALTER TABLE `language`
   ADD PRIMARY KEY (`language_id`);
 
 --
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`contract_no`);
+
+--
+-- Indexes for table `payment_type`
+--
+ALTER TABLE `payment_type`
+  ADD PRIMARY KEY (`payment_type_id`);
+
+--
+-- Indexes for table `permission_role`
+--
+ALTER TABLE `permission_role`
+  ADD PRIMARY KEY (`permission_id`);
+
+--
 -- Indexes for table `renewal_provision`
 --
 ALTER TABLE `renewal_provision`
@@ -350,7 +424,8 @@ ALTER TABLE `type`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_role` (`role_id`);
 
 --
 -- Indexes for table `user_role`
@@ -365,11 +440,6 @@ ALTER TABLE `vendor`
   ADD PRIMARY KEY (`vendor_id`),
   ADD UNIQUE KEY `vendor` (`vendor_id`);
 
-
-ALTER TABLE `notification`
-  ADD PRIMARY KEY (`contract_no`);
-
-
 --
 -- AUTO_INCREMENT for dumped tables
 --
@@ -379,61 +449,90 @@ ALTER TABLE `notification`
 --
 ALTER TABLE `category`
   MODIFY `category_id` int(2) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `country`
---
-ALTER TABLE `country`
-  MODIFY `country_id` int(2) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `currency`
 --
 ALTER TABLE `currency`
   MODIFY `currency_id` int(2) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `expiration`
 --
 ALTER TABLE `expiration`
-  MODIFY `expiration_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `expiration_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `issues`
 --
 ALTER TABLE `issues`
   MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `language`
 --
 ALTER TABLE `language`
   MODIFY `language_id` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment_type`
+--
+ALTER TABLE `payment_type`
+  MODIFY `payment_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `permission_role`
+--
+ALTER TABLE `permission_role`
+  MODIFY `permission_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
 --
 -- AUTO_INCREMENT for table `renewal_provision`
 --
 ALTER TABLE `renewal_provision`
   MODIFY `renewal_provision_id` int(2) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `service_delivery_manager`
 --
 ALTER TABLE `service_delivery_manager`
-  MODIFY `sdm_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `sdm_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `type`
 --
 ALTER TABLE `type`
   MODIFY `type_id` int(2) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `role_id` int(1) NOT NULL AUTO_INCREMENT;
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `vendor`
 --
 ALTER TABLE `vendor`
-  MODIFY `vendor_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `vendor_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_user_role` FOREIGN KEY (`role_id`) REFERENCES `user_role` (`role_id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
